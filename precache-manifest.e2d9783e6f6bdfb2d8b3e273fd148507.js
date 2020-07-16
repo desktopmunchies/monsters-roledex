6ac15f9dafeec04a5e81921ef2dd69286d32abfd
abc <abc@example.com>
2020-07-16 13:10:02 +0300

    Updates

---
+++ precache-manifest.e2d9783e6f6bdfb2d8b3e273fd148507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c687cb67cbaab74132c98db4ad3b8d",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "94222837da6dc77dcd3b",
+    "url": "/monsters-roledex/static/css/main.f5ac82a5.chunk.css"
+  },
+  {
+    "revision": "5baef3853c814a46c469",
+    "url": "/monsters-roledex/static/js/2.dd75d5d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.dd75d5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94222837da6dc77dcd3b",
+    "url": "/monsters-roledex/static/js/main.559f856d.chunk.js"
+  },
+  {
+    "revision": "cef29a3dcf903cb6b11a",
+    "url": "/monsters-roledex/static/js/runtime-main.de4da34a.js"
+  }
+]);
